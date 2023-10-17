cb0132f14de3bb72499d26c4fd038d07662df688
Vampir5050Dev <sanya.afonin33@gmail.com>
2023-10-17 14:14:35 +0300

    ASP/Movie

---
+++ ASP/Movie/Movie/TagHelpers/MoviePosterTagHelper.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using Movie.Models;
+
+namespace Movie.TagHelpers
+{
+    [HtmlTargetElement("img",Attributes ="poster")]
+    public class MoviePosterTagHelper:TagHelper
+    {
+        public Cinema Poster { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+
+            output.TagName = "img";
+            output.Attributes.Add("src",Poster.Poster);
+            output.Attributes.Add("onerror","event.target.src='/images/placeholder.png'");
+            output.Attributes.Add("class","card-img-top");
+            output.Attributes.Add("alt", "...");
+        }
+    }
+}
